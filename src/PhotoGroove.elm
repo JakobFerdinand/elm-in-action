ab43efb7967f0bc51b7e1d8fe5bef6eb6bb34198
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-11-15 12:10:12 +0100

    view tests

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,4 +1,4 @@
-port module PhotoGroove exposing (Model, Msg(..), Photo, initialModel, main, photoDecoder, update)
+port module PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, main, photoDecoder, photoFromUrl, update, urlPrefix, view)
 
 import Browser
 import Html exposing (Attribute, Html, button, canvas, div, h1, h3, img, input, label, node, text)
@@ -322,3 +322,8 @@ onSlide toMsg =
     at [ "detail", "userSlidTo" ] int
         |> Json.Decode.map toMsg
         |> on "slide"
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,14 +2,21 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes as Attr exposing (src)
 import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
-import PhotoGroove exposing (Model, Msg(..), initialModel, update)
+import PhotoGroove exposing (Model, Msg(..), Status(..), initialModel, photoFromUrl, update, urlPrefix, view)
 import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, tag, text)
 
 
-decoderTest : Test
-decoderTest =
+
+-- decoders
+
+
+photoDecoderTest : Test
+photoDecoderTest =
     fuzz2 string int "title defaults to (unitiled)" <|
         \url size ->
             [ ( "url", Encode.string url )
@@ -20,6 +27,10 @@ decoderTest =
                 |> Result.map .title
                 |> Expect.equal
                     (Ok "(untitled)")
+
+
+
+-- Update
 
 
 sliders : Test
@@ -40,3 +51,45 @@ testSlider description toMsg amountFromModel =
                 |> Tuple.first
                 |> amountFromModel
                 |> Expect.equal amount
+
+
+
+-- View
+
+
+noPhotosNoThumbnails : Test
+noPhotosNoThumbnails =
+    test "No thumbnails render when ther are no photos to render." <|
+        \_ ->
+            initialModel
+                |> PhotoGroove.view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "img" ]
+                |> Query.count (Expect.equal 0)
+
+
+thumbnailsWork : Test
+thumbnailsWork =
+    fuzz (Fuzz.intRange 1 5) "URLs reder as thumbnails" <|
+        \urlCount ->
+            let
+                urls : List String
+                urls =
+                    List.range 1 urlCount
+                        |> List.map (\num -> String.fromInt num ++ ".png")
+
+                thumbnailChecks : List (Query.Single msg -> Expectation)
+                thumbnailChecks =
+                    List.map thumbnailRendered urls
+            in
+            { initialModel | status = Loaded (List.map photoFromUrl urls) "" }
+                |> view
+                |> Query.fromHtml
+                |> Expect.all thumbnailChecks
+
+
+thumbnailRendered : String -> Query.Single msg -> Expectation
+thumbnailRendered url query =
+    query
+        |> Query.findAll [ tag "img", attribute (Attr.src (urlPrefix ++ url)) ]
+        |> Query.count (Expect.atLeast 1)

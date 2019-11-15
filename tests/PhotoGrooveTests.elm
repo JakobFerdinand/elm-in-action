13af6e455231fcaf3de111409985a732a6593d0c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-11-15 09:22:06 +0100

    Adding a first test

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,16 @@
+module PhotoGrooveTests exposing (..)
+
+import Expect exposing (Expectation)
+import Json.Decode exposing (decodeString)
+import PhotoGroove
+import Test exposing (..)
+
+
+decoderTest : Test
+decoderTest =
+    test "title defaults to (unitiled)" <|
+        \_ ->
+            """{ "url": "fruits.com", "size": 5 }"""
+                |> decodeString PhotoGroove.photoDecoder
+                |> Expect.equal
+                    (Ok { url = "fruits.com", size = 5, title = "(untitled)" })
